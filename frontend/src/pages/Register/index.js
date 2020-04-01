8bbcbd95c464de99d0083e668272510a5213a1f1
ekfuji <estefani.fujimoto@hotmail.com>
2020-04-01 09:34:53 -0300

    removendo backend e configurações no front iniciais para rodar a api

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import {Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import api from '../../services/api'
 import './styles.css';
 import logoImg from '../../assets/logo.svg'
+
+
 export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsApp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUF] = useState('');
+
+    async function handleRegister(event) {
+        // * O form por padrão ao receber o comando de submit recarrega a página,
+        // * Para evitar que isso ocorra pegamos o evento e utilizamos a função preventDefault
+        event.preventDefault();
+
+        const data = ({
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        });
+        console.log(data);
+        
+            const response = await api.post('ongs', data);
+
+            alert(`Seu ID de acesso: ${response.data.id}`);
+       
+        
+
+
+    }
+
+
     return (
         <div className="register-container">
             <div className="content">
@@ -11,22 +44,44 @@ export default function Register() {
                     <img src={logoImg} alt="Be The Hero"></img>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas
-                        a encontrarem os casos da sua ONG.
+                    a encontrarem os casos da sua ONG.
                     </p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
                     Voltar para o logon</Link>
                 </section>
-                <form>
-                    <input placeholder="Nome da ONG"/>
-                    <input type="email" placeholder="E-mail"/>
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={event => setName(event.target.value)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
+                    <input
+                        placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={event => setWhatsApp(event.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="Cidade"/>
-                        <input placeholder="UF" style={{ width: 80}} />
+                        <input
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={event => setCity(event.target.value)}
+                        />
+                        <input
+                            placeholder="UF"
+                            style={{ width: 80 }}
+                            value={uf}
+                            onChange={event => setUF(event.target.value)}
+                        />
                     </div>
-                    <button className="button">Cadastrar</button>
+                    <button type="submit" className="button">Cadastrar</button>
                 </form>
             </div>
 
